2bdf5a4ab146e7695a6898b368eb8af3455ee4e3
KAMORIKAREN <kamorikaren2019@gmail.com>
2024-04-28 03:44:48 -0700

    header file containing different sorting algorithms prototypes

--- sort.h
+++ sort.h
@@ -7,3 +7,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+/**
+ * listint - structure of a doubly linked list
+ * @n: integer value in the node
+ * @listint *prev: The previous value in the list
+ * @listint *next: The next value in the list
+ */
+typedef struct listint
+{
+	const int n;
+	struct listint *prev;
+	struct listint *next;
+} listint_t;
